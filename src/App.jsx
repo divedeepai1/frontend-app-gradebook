68840115ee20d05c1b8539adeac2bc56e86559f7
Ubuntu <ubuntu@ip-172-31-20-16.us-east-2.compute.internal>
2025-01-17 06:22:37 +0000

    test

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => (
     <Head />
     <div>
       <Header />
-<h1> TESTING GRADEBOOK IN PRO SERVER EDUCATING AI <h1/>
+<h1> TESTING GRADEBOOK IN PRO SERVER EDUCATING AI </h1>
       <main>
         <Routes>
           <Route
